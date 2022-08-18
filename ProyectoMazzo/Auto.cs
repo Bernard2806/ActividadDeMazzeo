9d257ba914eac9c5668f28f3fae348b5b11bd459
Bernard2806 <107374580+Bernard2806@users.noreply.github.com>
2022-08-18 15:56:56 -0300

    Add files via upload

---
+++ ProyectoMazzo/Auto.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoMazzo
+{
+    class Auto
+    {
+        private string Marca;
+        private int Ruedas;
+        private string Color;
+        private string Transmision;
+        private string Velocidad;
+        private double Precio;
+        public Auto()
+        {
+            this.Ruedas = 4;
+        }
+        public void setmarca(string valor)
+        {
+            this.Marca = valor;
+        }
+        public void setcolor(string Color)
+        {
+            this.Color = Color;
+        }
+        public void settransmision(string valor)
+        {
+            this.Transmision = valor;
+        }
+        public void setvelocidad(string velocidad)
+        {
+            this.Velocidad = velocidad; 
+        }
+        public void setprecio(double precio)
+        {
+            this.Precio = precio;
+        }
+    }
+}
---
+++ ProyectoMazzo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoMazzo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Auto auto1 = new Auto();
+            auto1.setmarca("bmw");
+            auto1.setcolor("Rojo");
+            auto1.settransmision("Automatico");
+            auto1.setvelocidad("360000000");
+            auto1.setprecio(2);
+        }
+    }
+}
